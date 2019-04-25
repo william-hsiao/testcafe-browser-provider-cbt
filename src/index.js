f0d7ef3b1de64a79957ca53bf3e31e1a572ce397
William Hsiao <william.hsiao.25@gmail.com>
2019-04-25 17:24:22 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -1,46 +1,54 @@
+import cbt from 'cbt_tunnels';
+
+const cbtOptions = {
+  username: process.env.CBT_TUNNELS_USERNAME,
+  authkey:  ''
+};
+
 export default {
-    // Multiple browsers support
-    isMultiBrowser: true,
+  // Multiple browsers support
+  isMultiBrowser: true,
 
+  // Required - must be implemented
+  // Browser control
+  async openBrowser(/* id, pageUrl, browserName */) {
+    cbt.start(cbtOptions, err => {});
+    throw new Error('Not implemented!');
+  },
 
-    // Required - must be implemented
-    // Browser control
-    async openBrowser (/* id, pageUrl, browserName */) {
-        throw new Error('Not implemented!');
-    },
+  async closeBrowser(/* id */) {
+    throw new Error('Not implemented!');
+  },
 
-    async closeBrowser (/* id */) {
-        throw new Error('Not implemented!');
-    },
+  // Optional - implement methods you need, remove other methods
+  // Initialization
+  async init() {
+    return;
+  },
 
+  async dispose() {
+    return;
+  },
 
-    // Optional - implement methods you need, remove other methods
-    // Initialization
-    async init () {
-        return;
-    },
+  // Browser names handling
+  async getBrowserList() {
+    throw new Error('Not implemented!');
+  },
 
-    async dispose () {
-        return;
-    },
+  async isValidBrowserName(/* browserName */) {
+    return true;
+  },
 
-    
-    // Browser names handling
-    async getBrowserList () {
-        throw new Error('Not implemented!');
-    },
+  // Extra methods
+  async resizeWindow(/* id, width, height, currentWidth, currentHeight */) {
+    this.reportWarning(
+      'The window resize functionality is not supported by the "crossbrowsertesting" browser provider.'
+    );
+  },
 
-    async isValidBrowserName (/* browserName */) {
-        return true;
-    },
-    
-
-    // Extra methods
-    async resizeWindow (/* id, width, height, currentWidth, currentHeight */) {
-        this.reportWarning('The window resize functionality is not supported by the "crossbrowsertesting" browser provider.');
-    },
-
-    async takeScreenshot (/* id, screenshotPath, pageWidth, pageHeight */) {
-        this.reportWarning('The screenshot functionality is not supported by the "crossbrowsertesting" browser provider.');
-    }
+  async takeScreenshot(/* id, screenshotPath, pageWidth, pageHeight */) {
+    this.reportWarning(
+      'The screenshot functionality is not supported by the "crossbrowsertesting" browser provider.'
+    );
+  }
 };
